4356c56a93e2ded0cd99198f44b1ea713efb07a7
dhanraj <djjangid376@gmail.com>
2024-06-23 02:23:17 +0530

    sign up with login

--- src/view/SignUp/apiFunctions.jsx
+++ src/view/SignUp/apiFunctions.jsx
@@ -1,16 +1,30 @@
-import { postApiData } from '@/services/ApiService';
-import { useNavigate } from 'react-router-dom';
+import { postApiData } from "@/services/ApiService";
+import { useNavigate } from "react-router-dom";
+import { actions as authActions } from "@/redux/slices/authSlice";
+import { useDispatch } from "react-redux";
 
 export const useRegister = () => {
-  const navigate = useNavigate()
-  const registerUser = async ({name, email, password}) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const registerUser = async ({ name, email, password }) => {
     try {
-      const response = await postApiData('/players/register', { name, email, password });
-      navigate('/login');
+      const response = await postApiData("/players/register", {
+        name,
+        email,
+        password,
+      });
+
+      localStorage.setItem("token", response?.data?.token);
+
+      dispatch(authActions.login(response?.data));
+      if (pathname === "/signup") {
+        navigate("/");
+      }
 
       return;
     } catch (error) {
-      console.error('Registration failed:', error);
+      console.error("Registration failed:", error);
       throw error;
     }
   };
