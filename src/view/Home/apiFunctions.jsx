256abc5d27432830b86ad8165dbf79e009a3bd48
dhanrajjangid22 <dhanraj.jangid@ignatiuz.com>
2024-06-25 18:11:29 +0530

    cart count slice and count on icon

--- src/view/Home/apiFunctions.jsx
+++ src/view/Home/apiFunctions.jsx
@@ -1,11 +1,12 @@
 import { getApiData } from "@/services/ApiService";
 import { actions as cartActions } from "@/redux/slices/cartSlice";
+import { useDispatch } from "react-redux";
 
 export const useHome = () => {
-  const getProducts = async (email, password) => {
+  const dispatch = useDispatch();
+  const getProducts = async () => {
     try {
       const response = await getApiData("/product");
-      //   dispatch(authActions.login(response?.data));
       return response;
     } catch (error) {
       console.error("Login failed:", error);
